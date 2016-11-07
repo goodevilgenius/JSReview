eef1c6c2550c601f5eabd42c2b19b6c6048eca1d
Dan Jones <dan@medology.com>
2016-11-07 06:33:56 -0600

    💎 Starting out

---
+++ script.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAgnjaeng_1v3Btvbaecxwuy_e5_P9wiOI",
+  authDomain: "jsreview-c8f89.firebaseapp.com",
+  databaseURL: "https://jsreview-c8f89.firebaseio.com",
+  storageBucket: "jsreview-c8f89.appspot.com",
+  messagingSenderId: "803592547685"
+};
+firebase.initializeApp(config);
